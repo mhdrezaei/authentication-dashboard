5e138dce538a0325a4556f687c1bc35698b5382f
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2023-02-26 13:43:34 +0330

    get body request data and some error handling

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(errorHandler);
 app.get("/", (req, res) => {
   res.status(200).json({ message: "wellcome to express" });
 });
