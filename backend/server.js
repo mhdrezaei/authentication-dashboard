666b63f3290ed1b2b78dadb4d468feccd48da6ed
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2023-02-26 14:06:34 +0330

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const { errorHandler } = require("./middleware/errorMiddleware");
-
+const connectDB = require("./config/db");
 const app = express();
+
+// connect to database
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
