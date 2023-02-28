b46360301c048bd1f157498a74e1e54a10238579
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2023-02-28 08:51:21 +0330

    cors options

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const app = express();
-
+// cors
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 // connect to database
 connectDB();
 
