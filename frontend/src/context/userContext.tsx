159b42d58fc18e6033e761531c773833a7e8ebd5
رضائی - محمد <rezaee-moha@tehran.iri>
2023-03-03 01:24:57 +0330

    add loading state and function to user context

--- frontend/src/context/userContext.tsx
+++ frontend/src/context/userContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, useState } from "react";
 import axios from "axios";
+import { toast } from 'react-toastify';
 
 type formDataType = {
   email: string;
@@ -7,8 +8,10 @@ type formDataType = {
 };
 export type userContextType = {
   isLogin: boolean;
+  isLoading : boolean;
   login: (formData: formDataType) => void;
-  confirmLogin: () => void;
+  confirmLogin : () => void;
+  loading: (status : boolean) => void;
 };
 
 const UserContext = createContext<userContextType | null>(null);
@@ -19,21 +22,51 @@ export const UserProvider = ({
   children: React.ReactElement;
 }) => {
   const [isLogin, setIsLogin] = useState(false);
+  const[isLoading , setIsLoading] = useState(false);
+
+  // Loading
+  const loading = (status : boolean) => {
+    setIsLoading(status);
+}
+
+
+// login
   const login = (formData: formDataType) => {
     axios
       .post("http://localhost:5000/api/users/login", formData)
       .then((response) => {
+        console.log(response)
+        if (!response.status) {
+          toast.error(`This is an HTTP error: The status is ${response.status}` ,{
+            position: "bottom-center",
+            theme: "colored"
+          })
+          return
+          }
         const data = response.data;
         const convertedData = JSON.stringify(data);
         setIsLogin(true);
-
+        // show successful logim message
+        toast.success(`wellcome ${data.name}` , {
+          position: "bottom-center",
+          theme: "colored"
+        })
         //set JWT token to local
         localStorage.setItem("user", convertedData);
 
         //set token to axios common header
         //  setAuthToken(token);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err)
+        toast.error(err,{
+          position: "bottom-center",
+          theme: "colored"
+        })
+      
+      });
+
+      
   };
 
   const confirmLogin = () => {
@@ -41,7 +74,7 @@ export const UserProvider = ({
   };
 
   return (
-    <UserContext.Provider value={{ isLogin, login, confirmLogin }}>
+    <UserContext.Provider value={{ isLogin, isLoading , loading , login, confirmLogin  }}>
       {children}
     </UserContext.Provider>
   );
