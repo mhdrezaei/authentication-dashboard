bbc2357d5140a0c34ab97826da15fb96ffdd158a
رضائی - محمد <rezaee-moha@tehran.iri>
2023-03-02 16:22:47 +0330

    create a function to check valid email address

---
+++ frontend/src/helper/email-validation.ts
@@ -0,0 +1,11 @@
+const emailValidation = (value : string) => {
+
+    return String(value).toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+
+}
+
+
+export default emailValidation;
