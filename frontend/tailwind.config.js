8e8b843cc6a4c3976a1332cb5733c9b49fd4094b
رضائی - محمد <rezaee-moha@tehran.iri>
2023-03-08 15:11:20 +0330

    add some color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,12 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        darkBlue: "hsl(217, 28%, 15%)",
-        darkBlue1: "hsl(218, 28%, 13%)",
-        darkBlue2: "hsl(216, 53%, 9%)",
-        darkBlue3: "hsl(219, 30%, 18%)",
+        darkBlue1: "hsl(211, 60%, 19%)",
+        darkBlue2: "hsl(219, 30%, 18%)",
+        darkBlue3: "hsl(217, 28%, 15%)",
+        darkBlue4: "hsl(218, 28%, 13%)",
+        darkBlue5: "hsl(216, 53%, 9%)",
         accentCyan: "hsl(176, 68%, 64%)",
         accentBlue: "hsl(198, 60%, 50%)",
+        accentBlueLight: "hsl(210, 100%, 70%)",
         lightRed: "hsl(0, 100%, 63%)",
       },
       fontFamily: {
