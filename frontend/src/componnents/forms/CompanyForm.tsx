a63012cb5a9a95eabd795aadba35222961fce390
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2023-03-09 17:07:35 +0330

    add fields

--- frontend/src/componnents/forms/CompanyForm.tsx
+++ frontend/src/componnents/forms/CompanyForm.tsx
@@ -1,93 +1,139 @@
-import React from 'react'
+import React from "react";
 
 const CompanyForm = () => {
   return (
     <div>
-
-<form className="w-full max-w-2xl">
-  <div className="flex flex-wrap -mx-3 mb-6">
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="form-label" htmlFor="grid-first-name">
-        First Name
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
-      <p className="text-red-500 text-xs italic">Please fill out this field.</p>
-    </div>
-    <div className="w-full md:w-1/2 px-3">
-      <label className="form-label" htmlFor="grid-last-name">
-        Last Name
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
-    </div>
-  </div>
-
-
-  <div className="flex flex-wrap -mx-3 mb-6">
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="form-label" htmlFor="grid-first-name">
-        First Name
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
-      <p className="text-red-500 text-xs italic">Please fill out this field.</p>
-    </div>
-    <div className="w-full md:w-1/2 px-3">
-      <label className="form-label" htmlFor="grid-last-name">
-        Last Name
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
-    </div>
-  </div>
-
-
-
-
-
-
-  <div className="flex flex-wrap -mx-3 mb-6">
-    <div className="w-full px-3">
-      <label className="form-label" htmlFor="grid-password">
-        Password
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
-      <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
-    </div>
-  </div>
-  <div className="flex flex-wrap -mx-3 mb-2">
-    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label className="form-label" htmlFor="grid-city">
-        City
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque" />
-    </div>
-    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label className="form-label" htmlFor="grid-state">
-        State
-      </label>
-      <div className="relative">
-        <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-          <option>New Mexico</option>
-          <option>Missouri</option>
-          <option>Texas</option>
-        </select>
-        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+      <form className="w-full max-w-3xl">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label className="form-label" htmlFor="grid-first-name">
+              First Name
+            </label>
+            <input
+              className="form-input    mb-3  "
+              id="grid-first-name"
+              type="text"
+              placeholder="Jane"
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label className="form-label" htmlFor="grid-last-name">
+              Last Name
+            </label>
+            <input
+              className="form-input"
+              id="grid-last-name"
+              type="text"
+              placeholder="Doe"
+            />
+          </div>
         </div>
-      </div>
+
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label className="form-label" htmlFor="grid-first-name">
+              Official Company Name
+            </label>
+            <input
+              className="form-input mb-3"
+              id="grid-first-name"
+              type="text"
+              placeholder="Google"
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label className="form-label" htmlFor="grid-last-name">
+              official Logo
+            </label>
+            <input
+              className="form-input"
+              id="grid-last-name"
+              type="file"
+              placeholder="Doe"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label className="form-label" htmlFor="grid-password">
+              Company Address
+            </label>
+            <input
+              className="form-input mb-3"
+              id="grid-password"
+              type="password"
+              placeholder="Iran , Tehran , Vanak Square , ..."
+            />
+            <p className="text-gray-600 text-xs italic">
+              Make it as long and as crazy as you'd like
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label className="form-label" htmlFor="grid-first-name">
+              Email Address
+            </label>
+            <input
+              className="form-input  mb-3  "
+              id="grid-first-name"
+              type="email"
+              placeholder="mycompany@gmail.com"
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label className="form-label" htmlFor="grid-last-name">
+              Phone number
+            </label>
+            <input
+              className="form-input \"
+              id="grid-last-name"
+              type="tel"
+              placeholder="Doe"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label className="form-label" htmlFor="grid-city">
+              Registration Number
+            </label>
+            <input
+              className="form-input      "
+              id="grid-city"
+              type="number"
+              placeholder="Albuquerque"
+            />
+          </div>
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label className="form-label" htmlFor="grid-city">
+              Zip Code
+            </label>
+            <input
+              className="form-input      "
+              id="grid-city"
+              type="number"
+              placeholder="Albuquerque"
+            />
+          </div>
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label className="form-label" htmlFor="grid-zip">
+              Registration Date
+            </label>
+            <input
+              className="form-input      "
+              id="grid-zip"
+              type="date"
+              placeholder="90210"
+            />
+          </div>
+        </div>
+      </form>
     </div>
-    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label className="form-label" htmlFor="grid-zip">
-        Zip
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="date" placeholder="90210" />
-    </div>
-  </div>
-</form>
+  );
+};
 
-
-
-
-    </div>
-  )
-}
-
-export default CompanyForm
+export default CompanyForm;
