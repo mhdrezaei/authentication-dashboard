9b34360ce0e3e4e2cf54e7dcee11c35d558e08e9
رضائی - محمد <rezaee-moha@tehran.iri>
2023-03-11 03:09:14 +0330

    create language context for multi language app

---
+++ frontend/src/languages/index.tsx
@@ -0,0 +1,11 @@
+import fa from './fa.json';
+import en from './en.json';
+import de from './de.json';
+
+export const dictionaryList = { en, fa, de };
+
+export const languageOptions = {
+  en: 'English',
+  tr: 'Türkçe',
+  de: 'Deutsch'
+};
