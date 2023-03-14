19b39ceb541c12b181f1c248a6d960299b0ed83b
رضائی - محمد <rezaee-moha@tehran.iri>
2023-03-14 09:51:21 +0330

    add deutsch language to config

--- frontend/src/i18n.tsx
+++ frontend/src/i18n.tsx
@@ -2,6 +2,7 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import homeFa from "../src/languages/fa.json";
 import homeEn from "../src/languages/en.json";
+import homeDe from "../src/languages/de.json";
 
 const resources = {
   en: {
@@ -9,6 +10,9 @@ const resources = {
   },
   fa: {
     home: homeFa,
+  },
+  de: {
+    home: homeDe,
   },
 };
 i18next.use(initReactI18next).init({
