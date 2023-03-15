e6a01c67f3c5dead2df0fe177f7a08cfbae6cb9e
رضائی - محمد <rezaee-moha@tehran.iri>
2023-03-15 06:00:21 +0330

    some change

--- frontend/src/componnents/common/SelectLang.tsx
+++ frontend/src/componnents/common/SelectLang.tsx
@@ -1,11 +1,13 @@
 import React, { ChangeEventHandler, useContext } from "react";
 import ThemeContext, { themeContextType } from "../../context/themeContext";
-
+import { useTranslation } from "react-i18next";
 const SelectLang: React.FC = () => {
+  const { t, i18n } = useTranslation("home");
   const { selectLang } = useContext(ThemeContext) as themeContextType;
   const handleChange: ChangeEventHandler<HTMLSelectElement> = (event) => {
     console.log(event.target.value);
     selectLang(event.target.value);
+    i18n.changeLanguage(event.target.value);
   };
   let lang;
   const currecntLang = window.localStorage.getItem("lang-theme");
