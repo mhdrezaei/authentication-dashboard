40f19c8ec144e2a24c9e9741be4270fa0c3d06d8
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2023-03-26 04:09:40 +0430

    add validation

--- frontend/src/componnents/LoginForm.tsx
+++ frontend/src/componnents/LoginForm.tsx
@@ -1,134 +1,84 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler } from "react-hook-form/dist/types/form";
 import UserContext, { userContextType } from "../context/userContext";
-import { toast } from 'react-toastify';
-import emailValidation from "../helper/email-validation";
-import Spinner from "./common/Spinner";
 
+import Input from "./common/Input";
+import SlideButton from "./common/SlideButton";
+import { FiLock, FiMail } from "react-icons/fi";
 
-type formState = {
-  email: string;
-  password: string;
-};
-function LoginForm() {
-  const { login , isLoading , loading } = useContext(UserContext) as userContextType;
-  const [formData, setFormData] = useState<formState>({
-    email: "",
-    password: "",
+const FormSchema = z.object({
+  email: z.string().email("Please enter a valid email adress."),
+  password: z
+    .string()
+    .min(6, "Password must be atleast 6 characters.")
+    .max(52, "Password must be less than 52 characters."),
+});
+
+type FormSchemaType = z.infer<typeof FormSchema>;
+
+const LoginForm: React.FunctionComponent = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormSchemaType>({
+    resolver: zodResolver(FormSchema),
   });
-  const [hasError , setHasError] = useState({hasEmailError : false , okEmail : false ,emailError : "input-error" ,hasPasswordError : false , okPassword : false ,passwordError : "input-error"})
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    loading(true)
-    if(formData.email === "" || formData.email.trim() === "" || formData.password === ""){
-      toast.error('All field are necessary!',{
-        position: "bottom-center",
-        theme: "colored"
-      })
-      setHasError((prevState) =>{
-        return {...prevState , hasEmailError : true , hasPasswordError : true , okEmail : false } 
+  const { login } = useContext(UserContext) as userContextType;
 
-       })
-       loading(false)
-      return
-    }
-    if(!emailValidation(formData.email)){
-      toast.error('Enter valid Email!',{
-        position: "bottom-center",
-        theme: "colored"
-      })
-      setHasError((prevState) =>{
-        return {...prevState , hasEmailError : true  , okEmail : false } 
-
-       })
-      loading(false)
-      return
-
-    }
-    setTimeout(() => {
-      login(formData);
-      loading(false)
-      setFormData({email : "" , password : ""})
-      setHasError({hasEmailError : true , okEmail : false ,emailError : "input-error" ,hasPasswordError : true , okPassword : false ,passwordError : "input-error"})
-    } , 2000)
+  const onSubmit: SubmitHandler<FormSchemaType> = (values) => {
+    console.log(values);
+    login(values)
   };
 
-  const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    const target = e.target as HTMLInputElement;
-    setFormData((prevState) => {
-      return {
-        ...prevState,
-        [target.id]: target.value,
-      };
-    });
-  };
-  const onBlurHandler :React.FocusEventHandler<HTMLInputElement>  = (event) =>{
-    const id = event.target.id;
-    const value = event.target.value;
-    if(id === "email"){
-       
-      if(!emailValidation(value)){
-        setHasError((prevState) =>{
-         return {...prevState , hasEmailError : true } 
-
-        })
-      }else if(emailValidation(value)){
-        setHasError((prevState) =>{
-          return {...prevState , hasEmailError : false , okEmail : true } 
-         })
-      }
-    
-    }if(id === "password"){
-      if(value === ""){
-      setHasError((prevState) =>{
-        return {...prevState , hasPasswordError : true } 
-       })
-      }else{
-        setHasError((prevState) =>{
-          return {...prevState , hasPasswordError : false , okPassword : true } 
-         })
-      }
-    }
-  }
   return (
     <section className="flex items-center justify-center w-screen h-screen bg-gradient-to-tr from-darkBlue to-accentBlue transition-all duration-1000">
-
-    <div className="flex items-center justify-start w-2/3 h-1/2 rounded-md shadow-md  login-form transition-all duration-1000">
-
-      {isLoading && <Spinner/> }
-      {!isLoading && <form
-        onSubmit={handleSubmit}
-        className="flex flex-col items-center justify-start w-1/2  space-y-5  border-slate-300 transition-all duration-1000"
+      <div className="flex items-center justify-start w-2/3 rounded-md shadow-md  login-form transition-all duration-1000">
+        <form
+          method="post"
+          className="m-8 w-2/5 text-sm"
+          onSubmit={handleSubmit(onSubmit)}
         >
-        <h3 className=" text-darkBlue text-4xl font-sans">
-          Wellcome to MainBrain
-        </h3>
-        <input
-          id="email"
-          name="email"
-          type="text"
-          placeholder="Email"
-          className={`${hasError.hasEmailError ? "input-error" : ""} ${hasError.okEmail ? "input-success" : ""} "text-lg p-3 px-6 rounded-md border-none outline-none focus:shadow-md transition-all duration-1000"`}
-          onChange={onChange}
-          onBlur={onBlurHandler}
-          />
-        <input
-          id="password"
-          name="password"
-          type="password"
-          placeholder="Password"
-          className={`${hasError.hasPasswordError ? "input-error" : ""} ${hasError.okPassword ? "input-success" : ""} text-lg p-3 px-6 rounded-md border-none outline-none focus:shadow-md transition-all duration-1000"`}
-          onChange={onChange}
-          onBlur={onBlurHandler}
+          <Input
+            name="email"
+            label="Email address"
+            type="text"
+            icon={<FiMail />}
+            placeholder="example@emaple.com"
+            register={register}
+            error={errors?.email?.message}
+            disabled={isSubmitting}
           />
 
-        <button className="bg-accentBlue text-white text-lg rounded-md p-2 px-12 hover:opacity-80 hover:cursor-pointer hover:shadow-md transition-all duration-1000">
-          Login
-        </button>
-      </form>
-}
-    </div>
-          </section>
+          <Input
+            name="password"
+            label="Password"
+            type="password"
+            icon={<FiLock />}
+            placeholder="***********"
+            register={register}
+            error={errors?.password?.message}
+            disabled={isSubmitting}
+          />
+          <div className="mt-2 hover:underline w-fit">
+            <a href="/forgot" className=" text-blue-600">
+              Forgot password ?
+            </a>
+          </div>
+          <SlideButton
+            type="submit"
+            text="Sign in"
+            slide_text="Secure sign in"
+            icon={<FiLock />}
+            disabled={isSubmitting}
+          />
+        </form>
+      </div>
+    </section>
   );
-}
+};
 
 export default LoginForm;
